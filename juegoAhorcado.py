fbc80735d9d518472940d692c418e6941db4124e
Edu Rosúa <eduardorosuae@gmail.com>
2022-06-21 20:31:31 +0200

    FEAT: pequeño juego del ahorcado en la terminal

---
+++ juegoAhorcado.py
@@ -0,0 +1,294 @@
+from random import choice
+
+lifes = 6
+
+def select_word(list_words):
+    return choice(list_words).upper()
+
+def hide_word(word):
+    hide_word = []
+    for letter in word:
+        hide_word.append('_ ')
+    show_board(hide_word)
+    return hide_word
+
+def is_game_finished():
+    if complete_word():
+        print(f'Congratulations!! you got the right word! --> { word.upper() }')
+    elif lifes <= 0:
+        print(f'Sorry, you loose :( the right word was --> { word.upper() }')
+
+def complete_word():
+    return (list(word) == list(hidden_word))
+
+def check_letter_in_word(player_letter):
+    global lifes
+    valid_letters = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'
+
+    for idx, letter in enumerate(word):
+        if player_letter not in valid_letters or player_letter == '-':
+            print('Please enter a valid letter')
+            break
+        elif player_letter in no_valid_letters:
+            print(f'You have already tried the letter { player_letter.upper() }. Try again.')
+            break
+        elif player_letter not in word:
+            lifes -= 1
+            print(f'The letter { player_letter } isn\'t in this word.')
+            break
+        elif letter == player_letter:
+            hidden_word[idx] = player_letter
+    no_valid_letters.append(player_letter)
+    is_game_finished()
+
+def show_board(hidden_word):
+    print(f'Lifes : { lifes }')
+    print(f'History of attempts : { no_valid_letters }\n')
+
+    print(hidden_word)
+
+def init_game():
+    global lifes
+    while lifes > 0 and not complete_word():
+        player_letter = input("Enter a letter: ").upper()
+        player_letter = '-' if (player_letter == '') else player_letter
+        print('\n')
+        check_letter_in_word(player_letter)
+        show_board(hidden_word)
+
+list_words = [
+    "Aardvark",
+    "Albatross",
+    "Alligator",
+    "Alpaca",
+    "Ant",
+    "Anteater",
+    "Antelope",
+    "Ape",
+    "Armadillo",
+    "Donkey",
+    "Baboon",
+    "Badger",
+    "Barracuda",
+    "Bat",
+    "Bear",
+    "Beaver",
+    "Bee",
+    "Bison",
+    "Boar",
+    "Buffalo",
+    "Butterfly",
+    "Camel",
+    "Capybara",
+    "Caribou",
+    "Cassowary",
+    "Cat",
+    "Caterpillar",
+    "Cattle",
+    "Chamois",
+    "Cheetah",
+    "Chicken",
+    "Chimpanzee",
+    "Chinchilla",
+    "Chough",
+    "Clam",
+    "Cobra",
+    "Cockroach",
+    "Cod",
+    "Cormorant",
+    "Coyote",
+    "Crab",
+    "Crane",
+    "Crocodile",
+    "Crow",
+    "Curlew",
+    "Deer",
+    "Dinosaur",
+    "Dog",
+    "Dogfish",
+    "Dolphin",
+    "Dotterel",
+    "Dove",
+    "Dragonfly",
+    "Duck",
+    "Dugong",
+    "Dunlin",
+    "Eagle",
+    "Echidna",
+    "Eel",
+    "Eland",
+    "Elephant",
+    "Elk",
+    "Emu",
+    "Falcon",
+    "Ferret",
+    "Finch",
+    "Fish",
+    "Flamingo",
+    "Fly",
+    "Fox",
+    "Frog",
+    "Gaur",
+    "Gazelle",
+    "Gerbil",
+    "Giraffe",
+    "Gnat",
+    "Gnu",
+    "Goat",
+    "Goldfinch",
+    "Goldfish",
+    "Goose",
+    "Gorilla",
+    "Goshawk",
+    "Grasshopper",
+    "Grouse",
+    "Guanaco",
+    "Gull",
+    "Hamster",
+    "Hare",
+    "Hawk",
+    "Hedgehog",
+    "Heron",
+    "Herring",
+    "Hippopotamus",
+    "Hornet",
+    "Horse",
+    "Human",
+    "Hummingbird",
+    "Hyena",
+    "Ibex",
+    "Ibis",
+    "Jackal",
+    "Jaguar",
+    "Jay",
+    "Jellyfish",
+    "Kangaroo",
+    "Kingfisher",
+    "Koala",
+    "Kookabura",
+    "Kouprey",
+    "Kudu",
+    "Lapwing",
+    "Lark",
+    "Lemur",
+    "Leopard",
+    "Lion",
+    "Llama",
+    "Lobster",
+    "Locust",
+    "Loris",
+    "Louse",
+    "Lyrebird",
+    "Magpie",
+    "Mallard",
+    "Manatee",
+    "Mandrill",
+    "Mantis",
+    "Marten",
+    "Meerkat",
+    "Mink",
+    "Mole",
+    "Mongoose",
+    "Monkey",
+    "Moose",
+    "Mosquito",
+    "Mouse",
+    "Mule",
+    "Narwhal",
+    "Newt",
+    "Nightingale",
+    "Octopus",
+    "Okapi",
+    "Opossum",
+    "Oryx",
+    "Ostrich",
+    "Otter",
+    "Owl",
+    "Oyster",
+    "Panther",
+    "Parrot",
+    "Partridge",
+    "Peafowl",
+    "Pelican",
+    "Penguin",
+    "Pheasant",
+    "Pig",
+    "Pigeon",
+    "Pony",
+    "Porcupine",
+    "Porpoise",
+    "Quail",
+    "Quelea",
+    "Quetzal",
+    "Rabbit",
+    "Raccoon",
+    "Rail",
+    "Ram",
+    "Rat",
+    "Raven",
+    "Red deer",
+    "Red panda",
+    "Reindeer",
+    "Rhinoceros",
+    "Rook",
+    "Salamander",
+    "Salmon",
+    "Sand Dollar",
+    "Sandpiper",
+    "Sardine",
+    "Scorpion",
+    "Seahorse",
+    "Seal",
+    "Shark",
+    "Sheep",
+    "Shrew",
+    "Skunk",
+    "Snail",
+    "Snake",
+    "Sparrow",
+    "Spider",
+    "Spoonbill",
+    "Squid",
+    "Squirrel",
+    "Starling",
+    "Stingray",
+    "Stinkbug",
+    "Stork",
+    "Swallow",
+    "Swan",
+    "Tapir",
+    "Tarsier",
+    "Termite",
+    "Tiger",
+    "Toad",
+    "Trout",
+    "Turkey",
+    "Turtle",
+    "Viper",
+    "Vulture",
+    "Wallaby",
+    "Walrus",
+    "Wasp",
+    "Weasel",
+    "Whale",
+    "Wildcat",
+    "Wolf",
+    "Wolverine",
+    "Wombat",
+    "Woodcock",
+    "Woodpecker",
+    "Worm",
+    "Wren",
+    "Yak",
+    "Zebra"
+]
+no_valid_letters = []
+
+print('GUESS THE ANIMAL!')
+word = select_word(list_words)
+hidden_word = hide_word(word)
+
+# For debugg purposes
+# print(f'DEBUGG: the right word is => { word }')
+
+init_game()
